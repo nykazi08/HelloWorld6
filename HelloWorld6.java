d30ebca6b91e15b238e9c527c3e7032fddfe0979
nykazi08 <ny_alam@yahoo.com>
2021-02-23 00:51:09 -0500

    Added HelloWorld Program

---
+++ HelloWorld6.java
@@ -0,0 +1,15 @@
+public class HelloWorld6
+{
+public static void main(String [] args)
+{
+	for(int i=1; i<10; i++)
+{
+	System.out.println(i+ " : Welcome to CI usnig Jenkins");
+}
+}
+}
+
+//cd C:\Users\ny_al\Desktop\Feb 2021_123\jenkins feb21
+//javac HelloWorld6.java 
+//java HelloWorld6
+//Command Execute Windows batch command
